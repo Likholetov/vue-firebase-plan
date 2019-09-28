c6808c065209ed9b85fec543380b535fb2e6b2a5
Alexandr Likholetov <likholetovalex@gmail.com>
2019-09-28 23:29:22 +0300

    main structure and firebase

---
+++ src/config/firebase-example.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,35 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dateFilter from './filters/date.filter'
+import messagePlugin from './utils/message.plugin'
 import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min.js'
 
-Vue.config.productionTip = false
+// firebase
+import firebaseConfig from './config/firebase.js'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+Vue.use(messagePlugin);
+Vue.use(Vuelidate);
+Vue.filter('date', dateFilter);
+
+firebase.initializeApp(firebaseConfig);
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app){
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+});
