5fb7ca655e084e7443a94d6c673a466fad7ca3d2
Alexandr Likholetov <likholetovalex@gmail.com>
2019-10-19 11:45:59 +0300

    profile validation

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,15 +4,27 @@
             <h3>Профиль</h3>
         </div>
 
-        <form class="form">
+        <form @submit.prevent="submitHandler" class="form">
             <div class="input-field">
                 <input
                         id="description"
                         type="text"
+                        v-model="name"
+                        :class="{invalid: $v.name.$dirty && !$v.name.required}"
                 >
                 <label for="description">Имя</label>
-                <span
-                        class="helper-text invalid">name</span>
+                <span class="helper-text invalid"
+                          v-if="$v.name.$dirty && !$v.name.required"
+                    >Поле не должно быть пустым</span>
+            </div>
+
+            <div class="switch">
+                <label>
+                    English
+                    <input type="checkbox">
+                    <span class="lever"></span>
+                    Русский
+                </label>
             </div>
 
             <button class="btn waves-effect waves-light" type="submit">
@@ -24,11 +36,44 @@
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+    import {required} from 'vuelidate/lib/validators';
+
     export default {
-        name: "Profile"
+        name: "Profile",
+        data: () => ({
+            name: ''
+        }),
+        validations: {
+            name: {required},
+        },
+        mounted(){
+            this.name = this.info.name;
+            setTimeout(() => {
+                M.updateTextFields();
+            }, 0);
+        },
+        computed: {
+            ...mapGetters(['info'])
+        },
+        methods: {
+            submitHandler(){
+                if (this.$v.$invalid){
+                    this.$v.$touch();
+                    return;
+                };
+
+                try {                   
+                } catch (e) {
+                    
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .switch{
+        margin-bottom: 2rem;
+    }
 </style>
