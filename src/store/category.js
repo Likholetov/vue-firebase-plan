c7e1836202d5179a7803efaf4d0ebdc655d5b754
Alexandr Likholetov <likholetovalex@gmail.com>
2019-11-13 13:43:41 +0300

    fix

--- src/store/category.js
+++ src/store/category.js
@@ -6,10 +6,12 @@ export default {
 			try {
 				const uid = await dispatch('getUid');
 				const categories =
-					(await firebase
-						.database()
-						.ref(`/users/${uid}/categories`)
-						.once('value')).val() || {};
+					(
+						await firebase
+							.database()
+							.ref(`/users/${uid}/categories`)
+							.once('value')
+					).val() || {};
 				return Object.keys(categories).map(key => ({
 					...categories[key],
 					id: key
@@ -23,11 +25,13 @@ export default {
 			try {
 				const uid = await dispatch('getUid');
 				const category =
-					(await firebase
-						.database()
-						.ref(`/users/${uid}/categories`)
-						.child(id)
-						.once('value')).val() || {};
+					(
+						await firebase
+							.database()
+							.ref(`/users/${uid}/categories`)
+							.child(id)
+							.once('value')
+					).val() || {};
 				return { ...category, id: id };
 			} catch (e) {
 				commit('setError', e);
